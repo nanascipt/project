45e0946c227975490d15deac80a541e03496e1c6
nanascipt <145854639+nanascipt@users.noreply.github.com>
2023-12-18 21:14:27 +0800

    Add files via upload

--- PROJECT/2drumpad.js
+++ PROJECT/2drumpad.js
@@ -1,64 +1,52 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const keyMap = {
-        "Digit1": "z1",
-        "Digit2": "z2",
-        "Digit3": "z3",
-        "Digit4": "z4",
-        "KeyQ": "zq",
-        "KeyW": "zw",
-        "KeyE": "ze",
-        "KeyR": "zr",
-        "KeyA": "za",
-        "KeyS": "zs",
-        "KeyD": "zd",
-        "KeyF": "zf",
-        "KeyZ": "zz",
-        "KeyX": "zx",
-        "KeyC": "zc",
-        "KeyV": "zv"
-    };
+    const keyData = [
+        { note: "z1", keyCode: "Digit1", file: "z1.wav" },
+        { note: "z2", keyCode: "Digit2", file: "z2.wav" },
+        { note: "z3", keyCode: "Digit3", file: "z3.wav" },
+        { note: "z4", keyCode: "Digit4", file: "z4.wav" },
+        { note: "zq", keyCode: "KeyQ", file: "zq.wav" },
+        { note: "zw", keyCode: "KeyW", file: "zw.wav" },
+        { note: "ze", keyCode: "KeyE", file: "ze.wav" },
+        { note: "zr", keyCode: "KeyR", file: "zr.wav" },
+        { note: "za", keyCode: "KeyA", file: "za.wav" },
+        { note: "zs", keyCode: "KeyS", file: "zs.wav" },
+        { note: "zd", keyCode: "KeyD", file: "zd.wav" },
+        { note: "zf", keyCode: "KeyF", file: "zf.wav" },
+        { note: "zz", keyCode: "KeyZ", file: "zz.wav" },
+        { note: "zx", keyCode: "KeyX", file: "zx.wav" },
+        { note: "zc", keyCode: "KeyC", file: "zc.wav" },
+        { note: "zv", keyCode: "KeyV", file: "zv.wav" }
+    ];
 
-    function playSound(note) {
-        const audio = new Audio(`${note}.wav`);
-        audio.currentTime = 0;
-        audio.play();
-    }
-
-    function activateKey(note) {
+    keyData.forEach(({ note, keyCode, file }) => {
+        const audio = new Audio(file);
         const keyElement = document.getElementById(note);
-        keyElement.style.boxShadow = "0px 0px";
-        keyElement.style.marginTop = "10px";
-    }
 
-    function deactivateKey(note) {
-        const keyElement = document.getElementById(note);
-        keyElement.style.boxShadow = "3px 3px 5px #8a8b8d";
-        keyElement.style.marginTop = "0px";
-    }
+        document.addEventListener('keydown', function (info) {
+            if (info.code === keyCode) {
+                audio.currentTime = 0;
+                audio.play();
+                keyElement.style.boxShadow = "0px 0px";
+                keyElement.style.marginTop = "10px";
+            }
+        });
 
-    document.addEventListener('keydown', function (info) {
-        const note = keyMap[info.code];
-        if (note) {
-            playSound(note);
-            activateKey(note);
-        }
-    });
+        document.addEventListener('keyup', function (info) {
+            if (info.code === keyCode) {
+                keyElement.style.boxShadow = "3px 3px 5px #8a8b8d";
+                keyElement.style.marginTop = "0px";
+            }
+        });
 
-    document.addEventListener('keyup', function (info) {
-        const note = keyMap[info.code];
-        if (note) {
-            deactivateKey(note);
-        }
-    });
-
-    // Add click event listeners for each key
-    Object.values(keyMap).forEach(note => {
-        const keyElement = document.getElementById(note);
         keyElement.addEventListener('click', function () {
-            playSound(note);
-            activateKey(note);
-            // Delay deactivation for a smooth visual effect
-            setTimeout(() => deactivateKey(note), 100);
+            audio.currentTime = 0;
+            audio.play();
+            keyElement.style.boxShadow = "0px 0px";
+            keyElement.style.marginTop = "10px";
+            setTimeout(() => {
+                keyElement.style.boxShadow = "3px 3px 5px #8a8b8d";
+                keyElement.style.marginTop = "0px";
+            }, 100);
         });
     });
 });
